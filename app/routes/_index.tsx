5154ab9cae655622565eec44dff40131e29392c3
mingderwang <admin@ming-ders-MacBook.local>
2024-04-11 12:35:44 -0700

    almost done

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,18 @@
-import { LoaderArgs } from "@remix-run/node";
+import { LoaderFunctionArgs } from "@remix-run/node";
 import { authenticator } from "../authenticator.server"
 import { useLoaderData } from "@remix-run/react";
 
-export function loader({request}: LoaderArgs ) {
+export function loader({request}: LoaderFunctionArgs ) {
   const user = authenticator.isAuthenticated(request)
   return { user }
 }
 
 export default function Index() {
   const { user } = useLoaderData<typeof loader>()
+  console.log(user)
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix Auth WebAuthn 😴😴!</h1>
       { user?.username ? <p>Hi {user.username}</p> : <p>Hi guest</p>}
     </div>
   );
--- app/session.server.ts
+++ app/session.server.ts
@@ -8,7 +8,7 @@ export const sessionStorage = createCookieSessionStorage({
     sameSite: "lax", // this helps with CSRF
     path: "/", // remember to add this so the cookie will work in all routes
     httpOnly: true, // for security reasons, make this cookie http only
-    secrets: ["s3cr3t"], // replace this with an actual secret
+    secrets: ["superSecret387123"], // replace this with an actual secret
     secure: process.env.NODE_ENV === "production", // enable this in prod only
   },
 });
